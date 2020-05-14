0fc2d8f8df44121ef7c52a11569c82ff54b040de
CreativeWrec <58211428+CreativeWrec@users.noreply.github.com>
2020-05-14 08:15:02 -0500

    Linq_UserStories

---
+++ LINQ/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+
+            //linq query syntax
+            //var results = words.Where(w => w.Contains("th"));
+            //foreach (var word in results)
+            //{
+            //    Console.WriteLine(results);
+            //}
+
+            //List<string> names = new List<string>() { "mike", "brad", "nevin", "zack", "mike" };
+
+            //foreach (var name in names)
+            //{
+            //    console.writeline(name);
+            //}
+
+            //List<string> distinct = names.distinct().tolist();
+
+            //foreach (var name in distinct)
+            //{
+            //    Console.WriteLine(name);
+            //}
+
+            var classGrades = new List<string>()
+            {
+                "80,100,92,89,65",
+                "93,81,78,84,69",
+                "73,88,83,99,64",
+                "98,100,66,74,55"
+
+            };
+
+            var average = classGrades.Average();
+        }
+    }
+}
---
+++ LINQ/WordsLibrary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQ
+{
+    class WordsLibrary
+    {
+        public List<Words> words = new List<Words>();
+        public string GetStringResult()
+        {
+            string word = Console.ReadLine();
+            return word;
+        }
+    }
+}
